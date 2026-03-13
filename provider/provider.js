d48720eb3de431017437f2a2c80ee1a4bf0c39e8
AEGIS-ASMO <266061250+AEGIS-ASMO@users.noreply.github.com>
2026-03-13 22:38:24 +0530

    Update provider.js

--- provider/provider.js
+++ provider/provider.js
@@ -3,164 +3,206 @@ const m3u8 = require('m3u8-parser');
 const logger = require('../logger');
 
 class Provider {
-  static LIMIT = 50;
-  static TYPE = 'movie';
-  static TRANSPORT_URL = '';
+
+  static LIMIT = 50
+  static TYPE = 'movie'
+  static TRANSPORT_URL = ''
 
   constructor(baseUrl, name, limit) {
-    this.baseUrl = baseUrl;
-    this.name = name;
-    this.limit = limit || Provider.LIMIT;
+    this.baseUrl = baseUrl
+    this.name = name
+    this.limit = limit || Provider.LIMIT
   }
 
   activate(id) {
-    return id.startsWith(this.baseUrl);
+    return id.startsWith(this.baseUrl)
   }
 
   page(skip) {
-    return Math.floor(skip / this.limit) + 1;
+    return Math.floor(skip / this.limit) + 1
   }
 
   async fetchHtml(url, referer = null) {
-    logger.info('fetching url', url);
+
+    logger.info('fetching url', url)
 
     try {
+
       const response = await axios.get(url, {
         headers: {
           'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36',
-          Accept: 'text/html,application/xhtml+xml',
-          Referer: referer || this.baseUrl,
+          Accept: '*/*',
+          Referer: referer || this.baseUrl
         },
-        timeout: 15000,
-      });
+        timeout: 15000
+      })
 
-      if (typeof response.data === "string") {
-  return response.data;
-}
+      return response.data
 
-return JSON.stringify(response.data);
-    } catch (error) {
-      console.error(error);
-      return '';
+    } catch (err) {
+      console.error(err)
+      return ''
     }
+
   }
 
   async handleCatalog(args) {
-    if (args.type === Provider.TYPE && this.activate(args.id)) {
-      logger.info({ args }, 'handleCatalog');
 
-      let url = this.getInitialUrl(args.id);
+    if (args.type === Provider.TYPE && this.activate(args.id)) {
+
+      logger.info({ args }, 'handleCatalog')
+
+      let url = this.getInitialUrl(args.id)
 
       if (args.extra) {
-        if (args.extra.search) url = this.handleSearch(args);
-        if (args.extra.genre) url = this.handleGenre(args);
-        if (args.extra.skip) url += this.handleSkip(url, args.extra.skip);
+
+        if (args.extra.search) url = this.handleSearch(args)
+        if (args.extra.genre) url = this.handleGenre(args)
+
+        if (args.extra.skip)
+          url += this.handleSkip(url, args.extra.skip)
+
       }
 
-      const html = await this.fetchHtml(url).catch(() => '');
-      const metas = this.getCatalogMetas(html);
+      const html = await this.fetchHtml(url)
 
-      logger.debug({ metasSize: metas.length }, 'catalog');
-      return Promise.resolve({ metas });
+      const metas = this.getCatalogMetas(html)
+
+      logger.debug({ metasSize: metas.length }, 'catalog')
+
+      return { metas }
+
     }
 
-    return Promise.resolve({ metas: [] });
+    return { metas: [] }
+
   }
 
   async handleMeta(args) {
+
     if (args.type === Provider.TYPE && this.activate(args.id)) {
+
       return this.getMetadata(args).then(meta => {
-        return { meta };
-      });
+        return { meta }
+      })
+
     }
 
-    return Promise.resolve({ meta: {} });
+    return { meta: {} }
+
   }
 
   async getMetadata(args) {
-    logger.info({ args }, 'getMetadata');
 
-    const { id } = args;
+    const { id } = args
 
-    return this.fetchHtml(id, id).then(html =>
-      this.parseVideoPage({ id, html })
-    );
+    return this.fetchHtml(id, id)
+      .then(html => this.parseVideoPage({ id, html }))
+
   }
 
   async handleStream(args) {
-    const { id } = args;
+
+    const { id } = args
 
     if (args.type === Provider.TYPE && this.activate(id)) {
-      logger.info({ args }, 'handleStream');
-      return this.processStreams(args);
+
+      logger.info({ args }, 'handleStream')
+
+      return this.processStreams(args)
+
     }
 
-    return Promise.resolve({ streams: [] });
+    return { streams: [] }
+
   }
 
   async getStreams(meta) {
+
     return this.fetchHtml(meta.videoPageUrl, meta.videoPageUrl)
       .then(content => this.parseM3u8(content))
-      .then(streams =>
-        streams.map(stream =>
-          this.transformStream(meta.videoPageUrl, stream)
-        )
-      )
-      .then(streams => {
-        return { streams };
-      });
+      .then(streams => streams.map(stream =>
+        this.transformStream(meta.videoPageUrl, stream)
+      ))
+      .then(streams => ({ streams }))
+
   }
 
   parseM3u8(content) {
-  try {
-    const parser = new m3u8.Parser();
-    parser.push(content);
-    parser.end();
 
-    const manifest = parser.manifest;
+    try {
 
-    if (!manifest.playlists || manifest.playlists.length === 0) {
-      return [{
-        type: "movie",
-        name: this.name + " stream",
-        url: "",
-      }];
+      const parser = new m3u8.Parser()
+
+      parser.push(content)
+      parser.end()
+
+      const manifest = parser.manifest
+
+      if (!manifest.playlists || manifest.playlists.length === 0) {
+
+        return [{
+          type: 'movie',
+          name: this.name + ' stream',
+          url: ''
+        }]
+
+      }
+
+      const streams = manifest.playlists.map(p => ({
+
+        url: p.uri,
+
+        resolution:
+          p.attributes && p.attributes.RESOLUTION
+            ? p.attributes.RESOLUTION.height + 'p'
+            : 'auto'
+
+      }))
+
+      streams.sort((a, b) => {
+
+        const ra = parseInt(a.resolution) || 0
+        const rb = parseInt(b.resolution) || 0
+
+        return rb - ra
+
+      })
+
+      return streams.map(stream => ({
+        type: 'movie',
+        name: `${this.name} ${stream.resolution}`,
+        url: stream.url
+      }))
+
+    } catch (err) {
+
+      console.error('parseM3u8 error', err)
+
+      return []
+
     }
 
-    const streams = manifest.playlists.map(p => ({
-      url: p.uri,
-      resolution:
-        p.attributes &&
-        p.attributes.RESOLUTION
-          ? p.attributes.RESOLUTION.height + "p"
-          : "auto"
-    }));
-
-    streams.sort((a, b) => {
-      const ra = parseInt(a.resolution) || 0;
-      const rb = parseInt(b.resolution) || 0;
-      return rb - ra;
-    });
-
-    return streams.map(stream => ({
-      type: "movie",
-      name: `${this.name} ${stream.resolution}`,
-      url: stream.url
-    }));
-
-  } catch (err) {
-    console.error("parseM3u8 error", err);
-    return [];
   }
-}
 
   transformStream(url, stream) {
+
     return {
       ...stream,
-      url: url.replace('hls.m3u8', '') + stream.url,
-    };
+      url: url.replace('hls.m3u8', '') + stream.url
+    }
+
   }
+
 }
 
-module.exports = Provider;
+module.exports = {
+
+  create() {
+    return new Provider('', 'Default')
+  },
+
+  Provider
+
+}
