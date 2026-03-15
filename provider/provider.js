b5389ad21601820d22577062950f437449b22393
AEGIS-ASMO <266061250+AEGIS-ASMO@users.noreply.github.com>
2026-03-15 16:41:53 +0530

    Update provider.js

--- provider/provider.js
+++ provider/provider.js
@@ -36,24 +36,33 @@ class Provider {
   }
 
   async fetchHtml(url) {
-    console.info('fetching url', url);
+  console.info('fetching url', url);
 
-    try {
-      const response = await client.get(url, {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122 Safari/537.36",
-          "Accept":
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-          "Accept-Language": "en-US,en;q=0.9",
-          "Referer": this.baseUrl,
-          "Origin": this.baseUrl,
-          "Connection": "keep-alive"
-        },
-        timeout: 15000
-      });
+  try {
+    const response = await client.get(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122 Safari/537.36",
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": this.baseUrl,
+        "Origin": this.baseUrl,
+        "Connection": "keep-alive"
+      },
+      timeout: 15000
+    });
+
+    return response.data;
+
+  } catch (error) {
+    console.error(error);
+    return '';
+  }
+}
 
 async fetchJson(url) {
+
   try {
     const response = await client.get(url, {
       headers: {
