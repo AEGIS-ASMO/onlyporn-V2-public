870bdc16b15a33ba036f4b2524fd78c1f6328eea
AEGIS-ASMO <266061250+AEGIS-ASMO@users.noreply.github.com>
2026-03-16 18:07:47 +0530

    Update sxyprn.js

--- provider/sxyprn.js
+++ provider/sxyprn.js
@@ -126,12 +126,14 @@ class SxyprnProvider extends Provider {
 
   let videoUrl = null;
 
+  // safer extraction from raw HTML
   const mgfsMatch = html.match(/data-mgfs=['"](\d+)['"]/);
 
   if (mgfsMatch) {
 
     const mgfs = mgfsMatch[1];
 
+    // rotate CDN server
     const cdn = Math.floor(Math.random() * 3) + 1;
 
     videoUrl =
