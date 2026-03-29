82948495468c59adeafec99252b95211620be6fc
AEGIS-ASMO <266061250+AEGIS-ASMO@users.noreply.github.com>
2026-03-29 19:52:17 +0530

    Update index.js

--- logger/index.js
+++ logger/index.js
@@ -1,19 +1,12 @@
 const pino = require('pino');
+
 const logger = new pino({
-  level: getLevel(),
+  level: 'debug',
   base: {
     pid: undefined,
     hostname: undefined,
   },
-  enabled: process.env.LOG_ENABLED === 'true'
+  enabled: true
 });
-
-function getLevel() {
-  if (process.env.LOG_LEVEL) {
-    return process.env.LOG_LEVEL
-  }
-
-  return process.env.NODE_ENV === 'production' ? 'info' : 'debug'
-}
 
 module.exports = logger;
